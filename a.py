a700b0c9f32ae7473ff2f0a018da7950a8587e33
yeop <hoyajjang0115@naver.com>
2025-10-22 08:04:21 +0900

    asdadasas

--- a.py
+++ a.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 data = pd.read_csv("health_dataset.csv")
-# print(data.head())
-print(data.)
+plt.plot(data['Age'],color='r')
+
+plt.show()
+
+
+
+
